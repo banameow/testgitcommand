1e02d5a2bf7a226954ece205a20ef565eb32a3eb
banana.meow <nawaphon.kit@student.mahidol.ac.th>
2025-08-20 18:52:36 +0700

    add number logic

--- TestGitCommand.java
+++ TestGitCommand.java
@@ -1,5 +1,8 @@
 public class TestGitCommand {
+    public static int number = 201;
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println(number > 200 ? "Number is greater than 200." : "Number is less than 200.");
     }
 }
