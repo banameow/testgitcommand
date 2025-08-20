5626b048a42def865b16f0e040ee7121b526d909
Waiyawat Ongapichart <Waiyawat.Ora@student.mahidol.ac.th>
2025-08-20 21:26:44 +0700

    test loop add

--- TestGitCommand.java
+++ TestGitCommand.java
@@ -6,7 +6,16 @@ public class TestGitCommand {
         System.out.println(number > 200 ? "Number is greater than 200." : "Number is less than 200.");
 
         print2Times();
+        time();
     }
+
+    public static int n = 3;
+    public static void time(){
+        for(int i = 0;i<n;i++){
+            System.out.print(i+" times\n");
+        }
+    }
+
     
     public static void print2Times() {
         System.out.println("Hi, this is 1 time(s)");
